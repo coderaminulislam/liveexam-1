070e68463eb4313ef67a834cfab7840249e3906f
coderaminulislam <aminulwife.taslima@gmail.com>
2024-03-15 23:27:16 +0600

    update

--- bin/ostad-live-exam.dart
+++ bin/ostad-live-exam.dart
@@ -1,4 +1,23 @@
 void main(){
-  print('This is me');
-  print('Live exam one');
+  List<Map<String, dynamic>> fruits =[
+    {'Name': 'Apple', 'Color': 'Red', 'Price': 2.5},
+    {'Name': 'Banana', 'Color': 'Yello', 'Price': 1.0},
+    {'Name': 'Grapes', 'Color': 'Purple', 'Price': 3.0},
+  ];
+
+  displayFruitDetails(fruits);
+
+
+}
+
+
+void displayFruitDetails(List<Map<String, dynamic>>fruits){
+   for( dynamic fruit in fruits){
+     print('Name:${fruit['Name']}, Color:${fruit['Color']}, Price: \$${fruit['Price']}');
+   }
+}
+
+void applyPriceDiscount(double discount){
+
+
 }
